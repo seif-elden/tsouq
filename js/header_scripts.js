5c182c814c68d1194ac10c5b70c9f867114b2800
Ahmed Abdel-Fattah Abdel-Moaty <47077630+ahmedabdelfattah50@users.noreply.github.com>
2021-04-24 18:08:10 +0200

    upload scripts css & html

---
+++ js/header_scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    var bodyWidth = $('body').innerWidth();
+
+    $('.NavbarMobile .navMobileOptions .navbar-toggler').click(function(){
+      if( $('.NavbarMobile .navMenuMobile').width() > 0 ){
+        $(".NavbarMobile .navMenuMobile").css('width','0');
+        $('.NavbarMobile .navMenuMobile a').hide();
+      } else {
+        if( bodyWidth >= 768 && bodyWidth <= 992 ){
+          $(".NavbarMobile .navMenuMobile").css('width','40%');
+        } else if(bodyWidth >= 558 && bodyWidth <= 767){
+          $(".NavbarMobile .navMenuMobile").css('width','38%');
+        } else if(bodyWidth >= 310 && bodyWidth <= 557){
+          $(".NavbarMobile .navMenuMobile").css('width','60%');
+        }
+  
+        $('.NavbarMobile .navMenuMobile a').css('display','flex');
+        $('body').css('background-colo'); 
+      }
+    });
+
+    $('.NavbarMobile .navMenuMobile .closebtn').click(function () {  
+      $(".NavbarMobile .navMenuMobile").css('width','0');
+      $('.NavbarMobile .navMenuMobile a').hide();
+    });
+
+
+      var navLarge = $('.navLarge'),
+          NavbarMobile = $('.NavbarMobile');
+
+    $(window).scroll(function(){
+      // navbar change
+      if($(this).scrollTop() > 300){
+
+        navLarge.addClass('navLargeTop');
+
+        NavbarMobile.addClass('NavbarMobileTop');
+
+      } else {
+        navLarge.removeClass('navLargeTop');
+        NavbarMobile.removeClass('NavbarMobileTop');
+      }      
+    });
+});
