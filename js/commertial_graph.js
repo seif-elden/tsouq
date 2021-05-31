452275bc76a9886f0c3ee967401884bb57fc2620
seif-elden <sofa6099@gmail.com>
2021-05-31 13:06:11 +0200

    fix

--- js/commertial_graph.js
+++ js/commertial_graph.js
@@ -1,45 +1,17 @@
-$("#day-date-end").click(function(){
-    this.max  = new Date().toISOString().split("T")[0];
+    if (screen.width <= 800) {
 
-    var date = new Date($('#day-date-start').val()).toISOString().split("T")[0];
-    this.min  =  date ;
+        $(".chart-goes-here").prepend('<canvas id="myChart" width="400" height="300"></canvas>')
 
+        
+    }else{
+
+            $(".chart-goes-here").prepend('<canvas id="myChart" width="400" height="200"></canvas>')
+
+    }
+
+$(".edit").click(function() {
+    $(this).find('span').find('i').toggle()
 })
 
-if (screen.width <= 800) {
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        },
-    });
-
-    $(".chart-goes-here").prepend('<canvas id="myChart" width="400" height="400"></canvas>')
-
-    
-    }else{
-        var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 4,
-            spaceBetween: 30,
-            pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            },
-        });
-        $(".chart-goes-here").prepend('<canvas id="myChart" width="400" height="200"></canvas>')
-
-    }
-
-
-    // Home number counterup
-    if($('.count-item').length){
-        $('.count-item h3').counterUp({
-            delay: 10,
-            time: 1000
-        });
-    }
-
-    // pop up
-    new WOW().init();
+// pop up
+new WOW().init();
